1e2e8e0d661a51bfeb3db58012734e9f05d59ec7
r2hu1 <hi@rahul.eu.org>
2024-09-24 20:30:24 +0530

    fix footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -67,20 +67,19 @@ function Footer() {
           </ul>
         </div> */}
 
-      <div className='flex items-center gap-8 mt-3'>
-        {/* <h3 className='text-lg font-bold'>Project For</h3> */}
-        <Image src='/img/SIH-logo.png' width={120} height={120} alt='logo' />
-        <Image src='/img/vsit-logo.png' width={160} height={160} alt='logo' />
-      </div>
-
       {/* <div>
           <h3 className='text-lg font-bold'>College</h3>
           <Image src='/img/vsit-logo.png' width={200} height={200} alt='logo' />
         </div> */}
       {/* </div> */}
-      <p className='mt-3 text-sm text-foreground/90 max-w-2xl'>
-        This is an hackathon project built by a team of 4. for <span className='text-blue-600'>Smart India Hackathon 2024</span> from colloge <span className='text-blue-600'>VSIT</span>.
+      <p className='mt-3 text-sm text-muted-foreground max-w-2xl'>
+        This is an hackathon project built by a team of 4. for <span className='text-foreground/70'>Smart India Hackathon 2024</span> from colloge <span className='text-foreground/70'>VSIT</span>.
       </p>
+      <div className='flex items-center gap-8 mt-3'>
+        {/* <h3 className='text-lg font-bold'>Project For</h3> */}
+        <Image src='/img/SIH-logo.png' width={120} height={120} alt='logo' />
+        <Image src='/img/vsit-logo.png' width={160} height={160} alt='logo' />
+      </div>
       <ul className='flex gap-3 mt-1'>
         <li>
           <Link href='/' className='text-sm font-medium underline-offset-2 underline text-foreground/80 hover:text-foreground'>
