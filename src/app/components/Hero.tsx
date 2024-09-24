edc708c9420445dfbfd89e8d21e851fa0a3224fd
r2hu1 <hi@rahul.eu.org>
2024-09-24 20:26:17 +0530

    fix hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -15,24 +15,19 @@ function Hero() {
     <div className='pt-9'>
       <HeroText />
       <div className='relative text-center p-8 rounded-lg'>
-      
-      
-     
-
-      <div className='flex gap-6 mt-8 justify-center'>
-        {['Active Farmers', 'Active Farmers', 'Active Farmers'].map((title, index) => (
-          <div key={index} className='w-[250px] h-[200px] bg-blue-200 rounded-xl flex items-center justify-center shadow-lg transition-transform transform hover:scale-105'>
-            <div className='text-center'>
-              <p className='text-lg font-medium mb-2'>{title}</p>
-              <p className='text-3xl font-bold mb-1'>
-                50+
-                <span className='text-sm text-gray-600 ml-2'>Some Text</span>
-              </p>
+        <div className='grid grid-cols-2 sm:grid-cols-3 gap-6 mt-8 md:justify-center'>
+          {['Active Farmers', 'Active Farmers', 'Active Farmers'].map((title, index) => (
+            <div key={index} className='w-full h-[200px] bg-blue-200 rounded-xl flex items-center justify-center shadow-lg transition-transform transform hover:scale-105'>
+              <div className='text-center'>
+                <p className='text-lg font-medium mb-2'>{title}</p>
+                <p className='text-3xl font-bold mb-1'>
+                  50+
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
